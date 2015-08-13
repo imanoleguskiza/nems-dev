96c5f7787a81733bf916d2533829e0838cc9849d
Frederic Dewinne <frederic@continuousphp.com>
2015-08-13 15:40:52 +0200

    add cloud formation template files

---
+++ resources/codedeploy/scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /var/www/html
+
+while [ ! -f /usr/local/etc/subsite/subsite.ini ]
+do
+  sleep 2
+done
+
+vendor/bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install
+chown -R www:www /var/www/html/*
