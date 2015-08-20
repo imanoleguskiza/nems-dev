05da567e8a29036f8e757af4a899501fb88b9e44
Frederic Dewinne <frederic@continuousphp.com>
2015-08-20 12:45:25 +0200

    fix mysql start issue

--- resources/codedeploy/scripts/install.sh
+++ resources/codedeploy/scripts/install.sh
@@ -6,5 +6,5 @@ do
   sleep 2
 done
 
-bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install >> /var/log/subsite/install.log
+bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install 2>&1 >> /var/log/subsite/install.log
 chown -R apache:apache /var/www/html/*
