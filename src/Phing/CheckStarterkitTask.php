4a8f38ff2cd42e09ad684c003730c7a040ea711c
Pieter Frenssen <pieter@frenssen.be>
2016-02-29 16:15:40 +0100

    MULTISITE-8999: Check if the remote tracking branch exists.

--- src/Phing/CheckStarterkitTask.php
+++ src/Phing/CheckStarterkitTask.php
@@ -2,6 +2,7 @@
 
 namespace NextEuropa\Phing;
 
+use GitWrapper\GitException;
 use GitWrapper\GitWrapper;
 
 require_once 'phing/Task.php';
@@ -54,8 +55,10 @@ class CheckStarterkitTask extends \Task {
     $this->checkRequiredProperties();
 
     // Add the remote for the starterkit if it doesn't exist yet.
-    if (!$this->subsiteRepository->hasRemote($this->starterkitRemote)) {
-      $this->log('Creating remote repository.');
+    $remote_branch = 'remotes/' . $this->starterkitRemote . '/' . $this->starterkitBranch;
+    $remote_exists = $this->subsiteRepository->hasRemote($this->starterkitRemote);
+    if (!$remote_exists) {
+      $this->log('Adding remote repository.');
       // Only track the given branch, and don't download any tags.
       $options = [
         '--no-tags' => TRUE,
@@ -64,21 +67,32 @@ class CheckStarterkitTask extends \Task {
       $this->subsiteRepository->addRemote($this->starterkitRemote, $this->starterkitRepository, $options);
     }
 
+    // Check if the tracking branch exists and create it if it doesn't.
+    try {
+      $this->subsiteRepository->run(array('rev-parse', $remote_branch));
+    }
+    catch (GitException $e) {
+      $this->log('Adding tracking branch.');
+      $this->subsiteRepository->remote('set-branches', '--add', $this->starterkitRemote, $this->starterkitBranch);
+    }
+
     // Fetch the latest changes.
     $this->log('Fetching latest changes.');
     $this->subsiteRepository->fetch($this->starterkitRemote);
 
     // Check if the latest commit on the remote is merged into the current
     // branch.
-    $remote = 'remotes/' . $this->starterkitRemote . '/' . $this->starterkitBranch;
-    $latest_commit = (string) $this->subsiteRepository->run(array('rev-parse', $remote));
-    $merge_base = (string) $this->subsiteRepository->run(array('merge-base @ ' . $remote));
+    $this->subsiteRepository->clearOutput();
+    $latest_commit = (string) $this->subsiteRepository->run(array('rev-parse', $remote_branch));
+    $merge_base = (string) $this->subsiteRepository->run(array('merge-base @ ' . $remote_branch));
 
     // If the latest commit on the remote is not merged into the current branch,
     // the repository is not up-to-date.
     if ($merge_base !== $latest_commit) {
       throw new \BuildException('The current branch is not up to date with the starterkit.');
     }
+
+    $this->log('The starterkit is up to date.');
   }
 
   /**
