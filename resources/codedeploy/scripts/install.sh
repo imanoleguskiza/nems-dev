b824a33b7913940cb8f9f583bd0035a09b0270b5
Pieter Frenssen <pieter@frenssen.be>
2016-03-11 12:09:46 +0200

    Attempt to install Curl on the acceptance environment.
    This is needed for the `chr` module which is part of the multisite standard.

--- resources/codedeploy/scripts/install.sh
+++ resources/codedeploy/scripts/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Install curl. This is needed for the `chr` module.
+# Todo: add this to the machine image instead.
+apt-get install php5-curl -qq
+
 cd /var/www/html
 
 while [ ! -f /usr/local/etc/subsite/subsite.ini ]
