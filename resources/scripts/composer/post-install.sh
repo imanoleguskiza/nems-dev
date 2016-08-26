17aa6ee188c6a82b51fa8aeeee2b964a394b0fc3
Alex Verbruggen <verbruggenalex@gmail.com>
2016-08-26 11:01:46 +0200

    Initial commit for qa-automation

--- resources/scripts/composer/post-install.sh
+++ resources/scripts/composer/post-install.sh
@@ -4,3 +4,9 @@
 if [ ! -h ".git/hooks/pre-push" ] ; then
   ln -s "../../vendor/pfrenssen/phpcs-pre-push/pre-push" ".git/hooks/pre-push"
 fi
+
+# Symlink the quality assurance task to its destination.
+if [ ! -h "src/Phing/QualityAssuranceTask.php" ] ; then
+  ln -s "../../vendor/ec-europa/qa-automation/QualityAssuranceTask.php" "src/Phing/QualityAssuranceTask.php"
+fi
+
